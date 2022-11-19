2fb1fac9ed720b8945bd54dd212e07e477c0703d
Mikołaj 'Holo' Barsznica <holo@frozent.pl>
2022-11-19 15:36:08 +0100

    Added splitscreen line

--- Assets/Scripts/Gameplay/Player.cs
+++ Assets/Scripts/Gameplay/Player.cs
@@ -3,31 +3,31 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    private Keyboard _Keyboard;
+    private Keyboard _keyboard;
     private const int SPEED = 8;
 
     public void Start()
     {
-	    _Keyboard = Keyboard.current;
+	    _keyboard = Keyboard.current;
     }
 
     public void Update()
     {
 	    var vector = Vector3.zero;
 	    
-	    if (_Keyboard.wKey.isPressed)
+	    if (_keyboard.wKey.isPressed)
 	    {
 		    vector += Vector3.forward * Time.deltaTime;
 	    }
-	    if (_Keyboard.sKey.isPressed)
+	    if (_keyboard.sKey.isPressed)
 	    {
 		    vector -= Vector3.forward * Time.deltaTime;
 	    }
-	    if (_Keyboard.dKey.isPressed)
+	    if (_keyboard.dKey.isPressed)
 	    {
 		    vector += Vector3.right * Time.deltaTime;
 	    }
-	    if (_Keyboard.aKey.isPressed)
+	    if (_keyboard.aKey.isPressed)
 	    {
 		    vector -= Vector3.right * Time.deltaTime;
 	    }
